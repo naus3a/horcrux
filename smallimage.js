c8a64afd536f9425ef349e0372d5cfc6cc287592
naus3a <naus3a@gmail.com>
2025-08-26 18:00:38 +0200

    the small image thing

---
+++ smallimage.js
@@ -0,0 +1,29 @@
+let shares = [];
+let slider;
+
+function preload(){
+  shares.push(loadImage("img/fdt_share0.png"));
+  shares.push(loadImage("img/fdt_share1.png"));
+}
+
+function setup() {
+  let w = 600;
+  let h = 450;
+  createCanvas(w,h);
+  
+  let maxSlider = shares[0].width+100;
+  slider = createSlider(0,maxSlider,maxSlider);
+  slider.position(10,10);
+  slider.size(500);
+}
+
+function draw() {
+  background(255);
+  text("drag me around",20,10);
+  push();
+  translate(10,55);
+  scale(1,1);
+  image(shares[0],0,0);
+  image(shares[1],slider.value(),0);
+  pop();
+}
